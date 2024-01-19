708a93801cdeb55115b9fc00c054d4bcbce10c69
Katya Raman <katyaraman@icloud.com>
2024-01-19 12:27:06 -0800

    added basic hello world demo app in ios

---
+++ team/Hello World Demo App/Hello World Demo App/Hello_World_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_World_Demo_AppApp.swift
+//  Hello World Demo App
+//
+//  Created by Katyayani G. Raman on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Hello_World_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
