102e08087d31f8af947e202b60acc97b4f6e413f
Emily <emilyhu916@gmail.com>
2024-01-25 13:27:36 -0800

    set up sql database

--- melo/db/musicdb.sql
+++ melo/db/musicdb.sql
@@ -1,6 +1,22 @@
+-- Database: musicdb
+
+-- DROP DATABASE IF EXISTS musicdb;
+
+CREATE DATABASE musicdb
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
 -- Table: public.User
 
 -- DROP TABLE IF EXISTS public."User";
+USE musicdb;
 
 CREATE TABLE IF NOT EXISTS public."User"
 (
