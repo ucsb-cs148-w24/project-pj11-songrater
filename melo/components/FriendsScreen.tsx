2ef9ea0c8abb4f194c28bf7a0ae4e2680d8642e4
LeyangNi <leyangni@ucsb.edu>
2024-03-06 22:12:04 -0800

    a little change

---
+++ melo/components/FriendsScreen.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { Pressable, 
+  StyleSheet, 
+  Text, 
+  View, 
+  Image,
+  Button,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { styles } from "./helper/Styles";
+import { typography } from "./helper/Typography";
+import { buttons } from "./helper/Buttons";
+import { Avatar, Divider, Card } from 'react-native-paper';
+
+
