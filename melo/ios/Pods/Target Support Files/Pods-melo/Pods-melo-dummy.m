4ed561126562171393129f3175620d5cdf49ed69
jeffmun <61306390+Jumshim@users.noreply.github.com>
2024-03-15 19:02:44 -0700

    deleted android and ios folders

--- melo/ios/Pods/Target Support Files/Pods-melo/Pods-melo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_melo : NSObject
-@end
-@implementation PodsDummy_Pods_melo
-@end
--- melo/ios/Pods/Target Support Files/RNFBDatabase/RNFBDatabase-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RNFBDatabase : NSObject
-@end
-@implementation PodsDummy_RNFBDatabase
-@end
